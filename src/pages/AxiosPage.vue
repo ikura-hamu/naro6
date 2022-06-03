ed1291ae6d519b0abd3b63a76c02a79ccfa5489d
ikura-hamu <harukikainuma@gmail.com>
2022-06-03 22:52:22 +0900

    copied and pasetes as it is written in MD

---
+++ src/pages/AxiosPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="sendGet">GET</button>
+    <button @click="sendPost">POST</button>
+    <button @click="sendPut">PUT</button>
+    <button @click="sendDelete">DELETE</button>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+const sendGet = () => axios.get("https://enrxfzkji6zo.x.pipedream.net");
+const sendPost = () =>
+  axios.post("https://enrxfzkji6zo.x.pipedream.net", {
+    traQID: "ikura-hamu",
+    grade: 22,
+  });
+const sendPut = () =>
+  axios.put("https://enrxfzkji6zo.x.pipedream.net", {
+    testData: { string: "string", number: 1 },
+  });
+const sendDelete = () =>
+  axios.delete("https://enrxfzkji6zo.x.pipedream.net", {
+    data: { po: 1 },
+  });
+</script>
